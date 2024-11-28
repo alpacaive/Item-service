bf5ac8e365eae3afa5f5bd6a0e1d0fa3a659d2cd
alpacaive <w6352800@naver.com>
2024-11-28 22:17:37 +0900

    상품 등록 처리 - @ModelAttribute

--- src/main/java/hello/item_service/web/item/basic/BasicItemController.java
+++ src/main/java/hello/item_service/web/item/basic/BasicItemController.java
@@ -50,6 +50,38 @@ public class BasicItemController {
     }
 
     /**
+     * @ModelAttribute("item") Item item
+     * model.addAttribute("item", item); 자동추가
+     */
+    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item, Model model) {
+        itemRepository.save(item);
+        // model.addAttribute("item", item); // 자동 추가, 생략 가능
+        return "basic/item";
+    }
+
+    /**
+     * @ModelAttribute name 생략 가능
+     * model.addAttribute(item); 자동 추가, 생략 가능
+     * 생략 시 model 에 저장되는 name 은 클래스명 첫글자만 소문자로 등록 Item -> item
+     */
+    @PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item) {
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
+    /**
+     * @ModelAttribute 자체 생략 가능
+     * model.addAttribute(item) 자동 추가
+     */
+    public String addItemV4(Item item) {
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
+
+    /**
      * 테스트용 데이터 추가
      */
     public void init() {
